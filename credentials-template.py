965f554ea0cb17893a50d58515f7e91a1d172802
maxoja <tawan.tpptnk@gmail.com>
2020-03-03 19:21:45 +0000

    [Add]: credentials file template

---
+++ credentials-template.py
@@ -0,0 +1,7 @@
+twitter_access_token = "xxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+twitter_access_token_secret = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxb"
+twitter_consumer_key = "bbbbbbbbbbbbbbbbbbbbbbbbb"
+twitter_consumer_secret = "bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb"
+
+mongo_url = '127.0.0.1:27017'
+mongo_db_name = 'local'
