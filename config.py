63f1d9baf73a0e3b53bf1e6cf98009ba210967ff
maxoja <tawan.tpptnk@gmail.com>
2020-03-03 19:33:43 +0000

    [Refact]: cleanup config.py

--- config.py
+++ config.py
@@ -1,7 +1,4 @@
-# topics = ["coronavirys", "wuhan", "valentinewithnobody", "valentinesday2020"]
+# for streaming crawler
 all_topics = []
 chosen_topics = ['corona virus','stock price', 'sonic movie', 'brexit']
-
-#boundingbox.klokantech.com
-# locations = [-85.97616108,36.9451024072,-85.8496475238,37.05198895]
 worldwide_locations = [-180,-90,180,90]
